c4d8769cefc20eee4e4e602aff376b63d737cd27
Ajal333 <ajapoos1999@gmail.com>
2020-04-18 13:24:45 +0530

    Almost Done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,28 +14,33 @@ module.exports = server => {
         res.render('login');
     });
 
-    server.post('/login' , (req,res,next) => {
-        passport.authenticate('local' , (err,user) => {
-            if(err) {
-                console.log('Login failed');
-                console.log(err);
+   
+    server.post("/login",  (req, res, next) => {
+        passport.authenticate("local", (err, user) => {
+            if (err) {
+                console.log("POST /login passport.authenticate()");
+                console.log(err)
+                req.flash("error", err.message);
                 return next(err);
             }
-            if(!user) {
-                console.log('Invalid username or password');
+            if (!user.isActive) {
+                req.flash("error", "Invalid username or password");
                 return res.redirect('/login');
             }
-            req.logIn(user,err => {
-                if(err) {
-                    console.log('Error while logging in!');
-                    console.log(err);
+            req.logIn(user, err => {
+                if (err) {
+                    console.log("POST /login req.logIN()");
+                    console.log(err)
+                    req.flash("error", err.message);
                     return next(err);
                 }
-                console.log(`${user.name} has logged in!`);
-                res.redirect('/user/'+user.name);
-            })
-        })
-    })
+                console.log(user.username + " Logged in.");
+                req.flash("success", "Good to see you again, " + user.username);
+               res.send('User Log-In sucessfull')
+            });
+        })(req, res, next);
+    });
+
 
     //Log-Out route
     server.get("/logout", (req, res) => {
@@ -46,72 +51,31 @@ module.exports = server => {
 
     //User Register Route
     server.get('/register',(req,res) => {
-        res.render('register');
+        res.render('signup');
     });
 
     server.post('/register' , (req,res,next) => {
-        let newUser = new User({
+        let token = registeringTokenGenerator();
+        let user = new User({
             name : req.body.name,
             email : req.body.mail,
             username : req.body.username,
-            registeringToken : registeringTokenGenerator(),
+            registeringToken : token,
         })
-        User.register(newUser,req.body.password , (err,user) => {
-            if(err) {
-                if (err.name === 'UserExistsError') {
-                    console.log("User already exits.");
+
+        if(req.body.password ==req.body.confpassword) {
+            console.log('Password Matching');
+            User.register(user,req.body.password , (err,user) => {
+                if(err) {
+                    if (err.name === 'UserExistsError') {
+                        console.log("User already exits.");
+                        return res.redirect("/register");
+                    }
+                    req.flash( "Something went wrong...");
+                    console.log("error", err);
                     return res.redirect("/register");
                 }
-                req.flash( "Something went wrong...");
-                console.log("error", err);
-                return res.redirect("/register");
-            }
-            let smtpTransport = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: 'your_email_id@gmail.com',
-                    pass: 'your_password'
-                }
-            });
-            let mailOptions = {
-                to: user.email,
-                from: 'Your Company Name <your_email_id@gmail.com>',
-                subject: 'Confirm Mail',
-                text:`Hey, 
-                The OTP for confirming your registration is ${registeringToken}.
-                Click on the following link to enter your OTP : http://${req.headers.host}/confirm/${registeringToken}
-                
-                Thank you!`
-            };
-            smtpTransport.sendMail(mailOptions, err => {
-                if (err) {
-                    
-                    console.log("Error in smtpTransport.sendMail()");
-                    return next(err)
-                }
-                console.log('Email sent.');
-                return next(null);
-            });
-        })
-    })
-
-    //Confirmation for registration
-    server.get('/confirm/:token' ,(req,res,next) => {
-        if(err) {
-            console.log('Error during mail confirmation');
-            return next(err);
-        }
-        User.findOne({registeringToken : req.params.token} , (err,user) => {
-            if(err) {
-                console.log('Error finding user with the OTP');
-                return next(err);
-            }
-            if(!user) {
-                console.log('The entered OTP is wrong');
-                req.flash('error', 'Wrong OTP entered');
-                return next();
-            }
-            passport.authenticate('local' , (err,user) => {
+            passport.authenticate("local" , (err,user) => {
                 console.log(user);
                 if(err) {
                     console.log("Error Occured during authentication");
@@ -121,14 +85,48 @@ module.exports = server => {
                     console.log('Cannot find user');
                     return res.redirect('/login')
                 }
-                req.logIn(user, err => {
-                    if(err) {
-                        return next(err);
+            })(req, res, next);
+        })  
+                let smtpTransport = nodemailer.createTransport({
+                    service: 'Gmail',
+                    auth: {
+                        user: 'visitthestars0010@gmail.com',
+                        pass: 'pass@882836'
                     }
-                    return res.redirect('/profiles/'+user.username);
-                })
-            })
+                });
+                let mailOptions = {
+                    to: req.body.mail,
+                    from: 'e-Xhibyte<visitthestars0010@gmail.com>',
+                    subject: 'Confirm Mail',
+                    text:`Hey, 
+                    The OTP for confirming your registration is ${token}.
+                    Click on the following link to enter your OTP : http://${req.headers.host}/confirm/${token}
+                    
+                    Thank you!`
+                };
+                smtpTransport.sendMail(mailOptions, err => {
+                    if (err) {
+                        
+                        console.log("Error in smtpTransport.sendMail()");
+                        return next(err)
+                    }
+                    console.log(`Email sent to ${req.body.name}`);
+                    return res.redirect('/login');
+                });
+        }
+      
+    })
 
+    //Confirmation for registration
+    server.get('/confirm/:token' ,(req,res,next) => {
+        User.findOneAndUpdate({registeringToken : req.params.token},{isActive : true} , err => {
+            if(err) {
+                console.log('Error finding user with the OTP');
+                return next(err);
+            }
+            res.send('Account creation successful.')
+            console.log('Account Created Sucessfully');
+            
         })
     })
 
@@ -161,25 +159,25 @@ module.exports = server => {
                             console.log('Error occured during password reset');
                             return followOn(err);
                         }
-                        return followOn(null,toke,user);
+                        return followOn(null,token,user);
                     })
                 })
             },
             (token , user , followOn ) => {
-                var smtpTransport = nodemailer.createTransport({
+                let smtpTransport = nodemailer.createTransport({
                     service: 'Gmail',
                     auth: {
-                        user: 'your_email_id@gmail.com',
-                        pass: 'your_password'
+                        user: 'visitthestars0010@gmail.com',
+                        pass: 'pass@882836'
                     }
                 });
-                var mailOptions = {
+                let mailOptions = {
                     to: user.email,
-                    from: 'Your Company Name <your_email_id@gmail.com>',
+                    from: 'e-Xhibyte <visitthestars0010@gmail.com>',
                     subject: 'Password Reset',
                     text: `Hey,
-                    The OTP to reset your password : ${registeringToken}.
-                    Click on this link to enter your OTP : http://${req.headers.host}/reset/${registeringToken}
+                    The OTP to reset your password : ${token}.
+                    Click on this link to enter your OTP : http://${req.headers.host}/reset/${token}
                    
                     Thank you! `
                 };
@@ -204,7 +202,7 @@ module.exports = server => {
         })
     })
 
-    server.get('/forget/:token' ,(req,res) => {
+    server.get('/reset/:token' ,(req,res) => {
         User.findOne({ passwordResetToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } },  (err, user) => {
             if (err) {
                 return console.log(err);
@@ -227,34 +225,39 @@ module.exports = server => {
                         console.log('OTP is incorrect');
                         res.render('reset',{message : 'The OTP is wrong or has expired'})
                     }
-                    if(req.body.password == req.body.confPassword) {
+                    if(req.body.password == req.body.confpassword) {
                         console.log('Password Matches');
                         user.setPassword(req.body.password, err => {
+                            console.log('Password Changed');
+                            
                             user.passwordResetToken = undefined;
                             user.resetPasswordExpires = undefined;
-                        })
+                        
                         user.save(err => {
                             req.logIn(user, err => {
+                                console.log('evadethi');
+                                
                                 followOn(err,user);
                             })
                         })
+                    })
                     }
                     else {
                         res.render('reset' , {token : req.params.token , message : 'Password Mismatch'})
                     }
                 })
-            },
+            },  
             (user,followOn) => {
-                var smtpTransport = nodemailer.createTransport({
+                let smtpTransport = nodemailer.createTransport({
                     service: 'Gmail',
                     auth: {
-                        user: 'your_mail@gmail.com',
-                        pass: 'your_password'
+                        user: 'visitthestars0010@gmail.com',
+                        pass: 'pass@882836'
                     }
                 });
-                var mailOptions = {
-                    to: user.username,
-                    from: '<CompanyName> your_mail@gmail.com',
+                let mailOptions = {
+                    to: user.email,
+                    from: 'e-Xhibyte <visitthestars0010@gmail.com>',
                     subject: 'Password Change Successful.',
                     text: `Hey,
                     You have sucessfully changed your password ${user.username}.
@@ -263,8 +266,11 @@ module.exports = server => {
                     Thank You.`
                 };
                 smtpTransport.sendMail(mailOptions, function (err) {
+                    if(err) {console.log(err);
+                    }
                     req.flash('success', 'Success! Your password has been changed.');
-                    done(err);
+                    console.log('Mail sucessfully sent');
+                    followOn(err);
                 });
             }
         ],err => {
@@ -272,6 +278,4 @@ module.exports = server => {
         })
     })
 }
-
-
 
